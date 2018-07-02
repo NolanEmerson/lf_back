a4ffe0ebf13bcf99eccaf56505f94577100a9cf9
Nolan <nemerson17@gmail.com>
2018-07-02 16:33:56 -0700

    Can now pull and display info from Twitter on load

--- main.js
+++ main.js
@@ -2,13 +2,37 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const ig = require('instagram-node').instagram();
+const Twitter = require('twitter');
+const dotenv = require('dotenv').load();
+const fs = require('fs');
 
+
+app.set('view engine', 'ejs');
+
+
+const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    bearer_token: process.env.TWITTER_BEARER_TOKEN
+});
+
+let twoots = [];
+
+client.get('search/tweets', {
+    q: '#spongebob'
+}, function(error, tweets, response) {
+    // console.log('Error: ', error);
+    // console.log('Tweets: ', tweets.statuses);
+    twoots = tweets.statuses;
+    // console.log('Response: ', response);
+});
 
 app.use('/assets', express.static('assets'));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.render('index', {tweets: twoots});
 });
 
 
